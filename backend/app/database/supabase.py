4f39e93ebb4e635099a4a7556e8ea5be2abc6239
Diar <diarselmani10@gmail.com>
2026-04-27 15:51:49 +0200

    fixs

--- backend/app/database/supabase.py
+++ backend/app/database/supabase.py
@@ -1,4 +1,6 @@
+# Change the first line to this:
 from supabase import create_client, Client 
+
 import os
 from dotenv import load_dotenv
 
@@ -7,4 +9,5 @@ load_dotenv()
 url = os.getenv("SUPABASE_URL")
 key = os.getenv("SUPABASE_KEY")
 
+# This now uses the function from the official library
 supabase: Client = create_client(url, key)
